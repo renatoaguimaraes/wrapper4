89f7fc7173f94f99e5e3e3be691cdf8d0189d149
Renato Guimaraes <renatoaguimaraes@gmail.com>
2022-09-04 12:27:31 -0300

    fix(plugin): dummy fallback plugin

--- pkg/plugin/plugin_loader.go
+++ pkg/plugin/plugin_loader.go
@@ -8,11 +8,23 @@ import (
 func Load(path string) PluginRunner {
 	p, err := plugin.Open(path)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println("plugin:", "load fail:", path, err)
+		return NewDummyPluginRunner()
 	}
-	plugin, err := p.Lookup("GetPlugin")
+	wp, err := p.Lookup("GetPlugin")
 	if err != nil {
-		log.Fatalln(err)
+		log.Println("plugin:", "GetPlugin function not found:", path)
+		return NewDummyPluginRunner()
 	}
-	return plugin.(func() interface{})().(PluginRunner)
+	gp, ok := wp.(func() interface{})
+	if !ok {
+		log.Println("plugin:", "GetPlugin can't be cast to 'func() interface{}'")
+		return NewDummyPluginRunner()
+	}
+	pr, ok := gp().(PluginRunner)
+	if !ok {
+		log.Println("plugin:", "GetPlugin doesn't return a PluginRunner instance")
+		return NewDummyPluginRunner()
+	}
+	return pr
 }
